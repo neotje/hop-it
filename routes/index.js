932b4be2284036d34f8d624a4b43ffa2ee138354
neo hop <neotje111@gmail.com>
2020-02-19 12:03:51 +0100

    added temporary variable to disable chat and account functions

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,22 @@ var router = express.Router();
 var config = require('config');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {    
-    res.render('index', { title: 'Hop-IT', path: req.path, session: req.session});
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Hop-IT', path: req.path, session: req.session, construct: config.get('construction')  });
 });
 
 router.get('/portfolio', function (req, res, next) {
-    res.render('portfolio', { title: 'Hop-IT Portfolio', path: req.path, projects: config.get("pages.portfolio.projects"), session: req.session});
+    res.render('portfolio', { 
+        title: 'Hop-IT Portfolio', 
+        path: req.path, 
+        projects: config.get("pages.portfolio.projects"), 
+        session: req.session, 
+        construct: config.get('construction') 
+    });
 });
 
 router.get('/contact', function (req, res, next) {
-    res.render('contact', { title: 'Hop-IT Contact', path: req.path, session: req.session});
+    res.render('contact', { title: 'Hop-IT Contact', path: req.path, session: req.session, construct: config.get('construction')  });
 });
 
 module.exports = router;
